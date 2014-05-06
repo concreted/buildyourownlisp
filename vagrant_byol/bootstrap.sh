e27c30216914add9b7dfb6e8005617cdf84e224b
Aric Huang <arichuang@gmail.com>
2014-05-05 20:52:21 -0700

    vagrant byol

--- vagrant_byol/bootstrap.sh
+++ vagrant_byol/bootstrap.sh
@@ -5,5 +5,10 @@ apt-get install -y git-core
 apt-get install -y emacs
 apt-get install -y libedit-dev
 
+
+git config user.name --global "Aric Huang"
+git config user.email --global "arichuang@gmail.com"
+git config credential.helper store
+
 rm -rf /var/www
 ln -fs /vagrant /var/www
