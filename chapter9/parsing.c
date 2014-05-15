78b17293703a1ee952183ec15bc6c94de36493bc
Aric Huang <arichuang@gmail.com>
2014-05-14 21:07:15 -0700

    chapter9 --> Reading Expressions

--- chapter9/parsing.c
+++ chapter9/parsing.c
@@ -29,29 +29,79 @@ void add_history(char* unused) {}
 #endif
 
 /* Setup */
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 typedef struct {
   int type;
   long num; 
-  int err;
+
+  char* err;
+  char* sym;
+  
+  int count;
+  struct lval** cell;
+
 } lval;
 
 /* lval_num constructor */
-lval lval_num(long x) {
-  lval v;
-  v.type = LVAL_NUM;
-  v.num = x;
+lval* lval_num(long x) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_NUM;
+  v->num = x;
   return v;
 }
 
 /* lval_err constructor */
-lval lval_err(int x) {
-  lval v;
-  v.type = LVAL_ERR;
-  v.err = x;
+lval* lval_err(char* m) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_ERR;
+  v->err = malloc(strlen(m) + 1);
+  strcpy(v->err, m);
   return v;
+}
+
+/* lval_sym constructor */
+lval* lval_sym(char* s) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym = malloc(strlen(s) + 1);
+  strcpy(v->sym, s);
+  return v;
+}
+
+/* A pointer to a new empty Sexpr lval */
+lval* lval_sexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
+/* Delete an lval */
+void lval_del(lval* v) {
+
+  switch (v->type) {
+    /* Do nothing special for number type */
+  case LVAL_NUM: break;
+
+    /* For Err or Sym free the string data */
+  case LVAL_ERR: free(v->err); break;
+  case LVAL_SYM: free(v->sym); break;
+
+    /* If Sexpr then delete all elements inside */
+  case LVAL_SEXPR:
+    for (int i = 0; i < v->count; i++) {
+      lval_del(v->cell[i]);
+    }
+    /* Also free the memory allocated to contain the pointers */
+    free(v->cell);
+    break;
+  }
+
+  /* Finally free the memory allocated for the "lval" struct itself */
+  free(v);
 }
 
 /* Print an lval */
