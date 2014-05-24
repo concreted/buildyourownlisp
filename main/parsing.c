1460b59e2374cbafc5192340c6d8f74eb944f89c
Aric Huang <arichuang@gmail.com>
2014-05-24 07:59:03 -0700

    Add lenv type to support environment 	struct lenv 	lenv helper functions

--- main/parsing.c
+++ main/parsing.c
@@ -44,7 +44,7 @@ typedef struct lenv lenv;
 
 typedef lval*(*lbuiltin)(lenv*, lval*);
 
-typedef struct lval {
+struct lval {
   int type;
   double num;
 
@@ -56,6 +56,14 @@ typedef struct lval {
   struct lval** cell;
 
 };
+
+struct lenv {
+  int count;
+  char** syms;
+  lval** vals;
+};
+
+/* lval Helper functions */
 
 /* lval_num constructor */
 lval* lval_num(double x) {
@@ -261,6 +269,65 @@ lval* lval_take(lval* v, int i) {
   lval_del(v);
   return x;
 }
+
+/* lenv Helper functions */
+lenv* lenv_new(void) {
+  lenv* e = malloc(sizeof(lenv));
+  e->count = 0;
+  e->syms = NULL;
+  e->vals = NULL;
+  return e;
+}
+
+void lenv_del(lenv* e) {
+  for (int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    lval_del(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
+lval* lenv_get(lenv* e, lval* k) {
+
+  /* Iterate over all items in environment */
+  for (int i = 0; i < e->count; i++) {
+    /* Check if the stored string matches the symbol string */
+    /* If it does, return a copy of the value */
+    if (strcmp(e->syms[i], k->sym) == 0) { return lval_copy(e->vals[i]); }
+  }
+  /* If no symbol found return error */
+  return lval_err("unbound symbol!");
+}
+
+void lenv_put(lenv* e, lval* k, lval* v) {
+
+  /* Iterate over all items in environment */
+  /* This is to see if variable already exists */
+  for (int i = 0; i < e->count; i++) {
+
+    /* If variable is found delete item at that position */
+    /* And replace with variable supplied by user */
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  /* If no existing entry found then allocate space for new entry */
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+  /* Copy contents of lval and symbol string into new location */
+  e->vals[e->count-1] = lval_copy(v);
+  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
+  strcpy(e->syms[e->count-1], k->sym);
+}
+
+/* Builtin functions */
 
 lval* builtin_head(lval* a) {
   /* Check Error Conditions */
